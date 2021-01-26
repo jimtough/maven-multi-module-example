d8fce2af901d3b02fee111e3e375f67b761c21c0
adultmalehuman <adultmalehuman@gmail.com>
2021-01-26 17:14:51 -0400

    Added @DataJpaTest in 'data' module for a repository

---
+++ data/src/test/java/com/jimtough/mmm/data/DummyApplication.java
@@ -0,0 +1,9 @@
+package com.jimtough.mmm.data;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This class exists only to keep @DataJpaTest annotation integration tests happy
+ */
+@SpringBootApplication
+public class DummyApplication {}
---
+++ data/src/test/java/com/jimtough/mmm/data/repository/IT_LanguageSpecificHelloRepository.java
@@ -0,0 +1,20 @@
+package com.jimtough.mmm.data.repository;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class IT_LanguageSpecificHelloRepository {
+
+	@Autowired
+	LanguageSpecificHelloRepository repo;
+
+	@Test
+	void xxx() {
+		assertFalse(repo.findOneByKey("xxxxxxxx").isPresent());
+	}
+
+}
